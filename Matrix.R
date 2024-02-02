12cbb3868ba123ff99e81cd2a938acff04d30749
Anshu Sarkar <anshusarkar11@gmail.com>
2024-02-02 19:14:45 +0530

    echo

---
+++ Matrix.R
@@ -0,0 +1,8 @@
+# In R matrix are also a datatype 
+# They are allocated using matrix() function
+# The function takes two arguments
+# One argument is a vector and another is 
+# number of rows or number of cloums which is 
+# 'ncolum=' or 'nrow='
+
+mat  <- matrix(c(1:10) , ncolum = 2 ) 
