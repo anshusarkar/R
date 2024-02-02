72e3b9a54b5a34d0659caa178524a87f72e1aef4
Anshu Sarkar <anshusarkar11@gmail.com>
2024-02-02 20:34:22 +0530

    echo

---
+++ Operators.R
@@ -0,0 +1,41 @@
+# Operators in R
+# Arithmatics operators + , -  , / , *
+
+quater_1_sales <- 74513.5
+
+quater_2_sales <- 84525.2
+
+total_sales <- (quater_1_sales + quater_2_sales)
+
+print(total_sales)
+
+# Logical Operators & , && , | , || , !
+
+X <- 12
+
+print(X < 10 && X < 9)
+
+print(X > 10 || X > 9)
+
+print(!X)
+
+# Conditional operators
+
+x <- 10
+
+if(x > 0){
+	  print("X is a positive number !")
+}
+else if ( x > 9 ){
+	  print("X is greater than 9 ")
+}
+else {
+	  print("Value isn't assinged to X")
+}
+
+
+
+
+
+
+
