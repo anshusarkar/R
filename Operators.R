559934de09aeef1933145feef0886bbd0b961893
Anshu Sarkar <anshusarkar11@gmail.com>
2024-02-04 11:42:17 +0530

    echo

--- Operators.R
+++ Operators.R
@@ -1,9 +1,9 @@
 # Operators in R
-# Arithmatics operators + , -  , / , *
+# Arithmetic operators + , -  , / , *
 
-quater_1_sales <- 74513.5
+quater_1_sales <- 74513.5 # leftwards assignment operator that can be  = too  also <<- works 
 
-quater_2_sales <- 84525.2
+84525.2 -> quater_2_sales # rightwards assignment operator even ->> works
 
 total_sales <- (quater_1_sales + quater_2_sales)
 
@@ -26,10 +26,9 @@ x <- 10
 if(x > 0){
 	  print("X is a positive number !")
 }
-else if ( x > 9 ){
+elseif( x > 9 ){
 	  print("X is greater than 9 ")
-}
-else {
+}else{
 	  print("Value isn't assinged to X")
 }
 
