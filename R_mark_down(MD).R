83bd9339f7196e7cf97d5c84310106d5e20c7f4a
Anshu Sarkar <anshusarkar11@gmail.com>
2024-02-14 19:56:15 +0530

    echo

--- R_mark_down(MD).R
+++ R_mark_down(MD).R
@@ -3,3 +3,11 @@
 
 install.packages("rmarkdown")
 
+# IF their is an error install the additional package called
+# 'sass'
+# After installing the package the .Rmd files would get 
+# converted into HTML files 
+# which is helpful for creating summaries of the analysis done
+# to do that hit the "Kint" option 
+# from which the file can be converted into either word , HTML or PDF
+
